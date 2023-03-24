336d6ae59b14e821cce2ecef09c94ab2159a1451
ryan-aristosa <ryan.aristosa@softvision.com>
2023-03-24 18:22:01 +0800

    Cache big performance gains

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,54 +1,31 @@
 import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return {
-            path: args.path,
-            namespace: 'a',
-          };
-        }
-
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-            namespace: 'a',
-          };
-        }
-
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return {
-          path: `https://unpkg.com/${args.path}`,
+          path: 'index.js',
           namespace: 'a',
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: `
-              import React from 'react';
-              console.log(React);
-            `,
-          };
-        }
-
-        const { data, request } = await axios.get(args.path);
-        console.log(request);
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          loader: 'jsx',
-          contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+          namespace: 'a',
+        };
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          path: `https://unpkg.com/${args.path}`,
+          namespace: 'a',
         };
       });
     },
