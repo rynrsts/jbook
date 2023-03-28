6ab18b2a174e32f3874279eca02c793b0d6c4e23
ryan-aristosa <ryan.aristosa@softvision.com>
2023-03-28 15:55:51 +0800

    Handle untrusted code execution safely

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: 'https:unpkg.com/esbuild-wasm@0.8.27/esbuild-wasm',
+      wasmURL: 'https:unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
 
@@ -25,6 +25,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -35,8 +37,30 @@ const App = () => {
         global: 'window',
       },
     });
-    setCode(result.outputFiles[0].text);
+
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              throw err;
+            }
+          }, false);
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -47,7 +71,12 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        srcDoc={html}
+        sandbox="allow-scripts"
+        ref={iframe}
+        title="preview"
+      />
     </div>
   );
 };
