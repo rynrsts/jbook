387e80107d3d4b83d01f63465f21b3f26157a71f
ryan-aristosa <ryan.aristosa@softvision.com>
2023-04-05 16:56:45 +0800

    Add cumulative code execution

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import './cell-list.css';
 import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
 import { useTypedSelector } from '../hooks/use-typed-selector';
@@ -16,7 +17,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell previousCellId={null} forceVisible={cells.length === 0} />
       {renderedCells}
     </div>
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -4,6 +4,7 @@ import CodeEditor from './code-editor';
 import Preview from './preview';
 import Resizable from './resizable';
 import { useActions } from '../hooks/use-actions';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import { Cell } from '../state';
 
@@ -14,23 +15,23 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  console.log(bundle);
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return;
     }
 
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.id, cell.content, createBundle]);
+  }, [cell.id, cumulativeCode, createBundle]);
 
   return (
     <Resizable direction="vertical">
